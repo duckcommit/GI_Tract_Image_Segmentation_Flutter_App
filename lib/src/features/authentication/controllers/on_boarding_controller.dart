b719f3586b6ef3dfcd96a9b32a3d5068beb331ee
Vyshnav Ajith <puthen1977@gmail.com>
2023-11-04 05:01:41 +0530

    Create on_boarding_controller.dart
    Adding liquid display here.

---
+++ lib/src/features/authentication/controllers/on_boarding_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'package:liquid_swipe/PageHelpers/LiquidController.dart';
+
+class OnBoardingController extends GetxController{
+  
+  final controller=LiquidController();
+  RxInt currentPage =0.obs; 
+}
