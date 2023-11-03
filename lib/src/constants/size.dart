663ccca4377a35990edad929a49db2ad9988fa58
Vyshnav Ajith <puthen1977@gmail.com>
2023-11-04 04:58:07 +0530

    Create size.dart
    Unified Size

---
+++ lib/src/constants/size.dart
@@ -0,0 +1,7 @@
+const tDefaultSize=30.0;
+const tSplashContainerSize=30.0;
+const tButtonHeight = 15.0;
+const tFormHeight=30.0;
+
+const tDashboardPadding = 20.0;
+const tDashboardCardPadding = 20.0;
