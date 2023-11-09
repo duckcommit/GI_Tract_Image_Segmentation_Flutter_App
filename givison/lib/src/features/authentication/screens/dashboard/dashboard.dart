5bd815228b22c7d67d7c56b677d9f676eb0533cf
VYSHNAV AJITH <puthen1977@gmail.com>
2023-11-09 14:14:37 +0530

    fix: Improved Performance of Dashboard

--- givison/lib/src/features/authentication/screens/dashboard/dashboard.dart
+++ givison/lib/src/features/authentication/screens/dashboard/dashboard.dart
@@ -4,7 +4,6 @@ import "package:givison/src/constants/image_strings.dart";
 import "package:givison/src/constants/size.dart";
 import "package:givison/src/constants/text_strings.dart";
 import 'package:givison/src/features/authentication/screens/dashboard/profile.dart';
-import 'package:givison/src/repository/authentication_repository/authentication_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:givison/src/features/authentication/screens/on_boarding/on_boarding_screen.dart';
 import 'package:givison/src/features/authentication/screens/dashboard/error.dart';
@@ -15,40 +14,31 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 import 'package:givison/src/features/authentication/screens/dashboard/DataModel.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:givison/src/features/authentication/screens/dashboard/profile.dart';
 
-class Dashboard extends StatefulWidget{
-  const Dashboard({Key? key}): super(key: key);
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
 
-  
   @override
-  State<Dashboard> createState()=> _DashboardState();
+  State<Dashboard> createState() => _DashboardState();
 }
 
-Future<DataModel> submitData(String imageUrl)async{
-  
+Future<DataModel> submitData(String imageUrl) async {
   final uri = Uri.parse('http://4.154.41.19:8000/predict');
-    Map<String,String> headers = {'Content-Type':'application/json'};
-final msg = jsonEncode({"image": imageUrl});
+  Map<String, String> headers = {'Content-Type': 'application/json'};
+  final msg = jsonEncode({"image": imageUrl});
 
-  var response = await http.post(uri, 
-  body: msg,
-  headers:headers
-  );
-  print(imageUrl);
-  var data = response.body;
+  var response = await http.post(uri, body: msg, headers: headers);
   var jsonData = json.decode(response.body);
-  var key="image_url";
+  var key = "image_url";
   var valueVar = jsonData[key];
   //downloadfile(valueVar);
-  var time=DateTime.now().millisecondsSinceEpoch;
+  var time = DateTime.now().millisecondsSinceEpoch;
   var path = "/storage/emulated/0/Download/image-$time.png";
-  var file =File(path);
+  var file = File(path);
   var res = await http.get(Uri.parse(valueVar));
   file.writeAsBytes(res.bodyBytes);
-  if (response.statusCode==200){
+  if (response.statusCode == 200) {
     String responseString = response.body;
     return dataModelFromJson(responseString);
   }
@@ -56,122 +46,139 @@ final msg = jsonEncode({"image": imageUrl});
   return dataModelFromJson(responseString);
 }
 
-class _DashboardState extends State<Dashboard>{
-  final _auth = FirebaseAuth.instance;
-  DataModel? _dataModel; 
+class _DashboardState extends State<Dashboard> {
+  // ignore: unused_field
+  DataModel? _dataModel; //used in line 156
   File? image;
-  String imageUrl='';
+  String imageUrl = '';
 
-  bool isloading=false;
+  bool isloading = false;
 
   @override
-  Widget build(BuildContext context){
-    final size= MediaQuery.of(context).size;
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        leading: ClickableIcon(),
-        title: Text(tAppNameBar, style: Theme.of(context).textTheme.displayMedium),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        actions: [
-          Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: tCardBgColor),
-            child: IconButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> ProfileScreen()));},
-            icon: const Image(image: AssetImage(tman))),
-          )
-        ]
-      ),
-      body:SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(tDashboardPadding),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children:[
-              Text(tDashboardTitleMain, style:Theme.of(context).textTheme.displayLarge),
-              const SizedBox(height: tDashboardPadding*1),
-              Text(tDashboardTitle, style:Theme.of(context).textTheme.bodyMedium),
-              const SizedBox(height: tDashboardPadding*2),
-              Image(image: AssetImage(tupload), height: size.height*0.4,),
-              SizedBox(
-                        width:double.infinity,
-                        child: ElevatedButton(
-                          onPressed: ()async{
-                            
-                            ImagePicker imagePicker=ImagePicker();
-                            final image = await imagePicker.pickImage(source:ImageSource.gallery);
-                            if (image ==null) return;
-                            print("Hi");
-                            final imageTemporary = File(image.path);
-                            String uniqueFileName=DateTime.now().millisecondsSinceEpoch.toString();
-                            Reference referenceRoot=FirebaseStorage.instance.ref();
-                            Reference referenceDirImages=referenceRoot.child("images");
-                            Reference referenceImageToUpload=referenceDirImages.child(uniqueFileName);
+        appBar: AppBar(
+            leading: ClickableIcon(),
+            title: Text(tAppNameBar,
+                style: Theme.of(context).textTheme.displayMedium),
+            centerTitle: true,
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            actions: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: tCardBgColor),
+                child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ProfileScreen()));
+                    },
+                    icon: const Image(image: AssetImage(tman))),
+              )
+            ]),
+        body: SingleChildScrollView(
+          child: Container(
+              padding: const EdgeInsets.all(tDashboardPadding),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(tDashboardTitleMain,
+                        style: Theme.of(context).textTheme.displayLarge),
+                    const SizedBox(height: tDashboardPadding * 1),
+                    Text(tDashboardTitle,
+                        style: Theme.of(context).textTheme.bodyMedium),
+                    const SizedBox(height: tDashboardPadding * 2),
+                    Image(
+                      image: const AssetImage(tupload),
+                      height: size.height * 0.4,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            ImagePicker imagePicker = ImagePicker();
+                            final image = await imagePicker.pickImage(
+                                source: ImageSource.gallery);
+                            if (image == null) return;
+                            String uniqueFileName = DateTime.now()
+                                .millisecondsSinceEpoch
+                                .toString();
+                            Reference referenceRoot =
+                                FirebaseStorage.instance.ref();
+                            Reference referenceDirImages =
+                                referenceRoot.child("images");
+                            Reference referenceImageToUpload =
+                                referenceDirImages.child(uniqueFileName);
 
-                            try{
-                            await referenceImageToUpload.putFile(File(image.path));
-                            imageUrl=await referenceImageToUpload.getDownloadURL();
-                            print("Hi");
-                            print(imageUrl);
-                            
-                            }catch(error){
+                            try {
+                              await referenceImageToUpload
+                                  .putFile(File(image.path));
+                              imageUrl =
+                                  await referenceImageToUpload.getDownloadURL();
+                            } catch (error) {}
+                            ;
+                          },
+                          child: Text(tUpload.toUpperCase())),
+                    ),
+                    const SizedBox(height: tDashboardPadding * 2),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            onSurface: Colors.blue,
+                          ),
+                          onPressed: () async {
+                            if (imageUrl.isEmpty) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ErrorScreen()));
+                              isloading = false;
+                            } else {
+                              setState(() {
+                                isloading = true;
+                              });
+                              Future.delayed(const Duration(seconds: 20), () {
+                                setState(() {
+                                  isloading = false;
+                                });
+                                Fluttertoast.showToast(msg: "Downloaded!");
+                              });
 
-                            };
-                          }, 
-                          child: Text(tUpload.toUpperCase())
-                        ),
-                      ),
-              const SizedBox(height: tDashboardPadding*2),       
-              SizedBox(
-                        width:double.infinity,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(onSurface: Colors.blue,),
-                          onPressed: ()async{
-                            if (imageUrl.length==0){
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=> ErrorScreen()));
-                              isloading=false;
-                            }
-                            else{
-                              setState((){
-                              isloading=true;
-                              });
-                              Future.delayed(Duration(seconds: 20),(){
-                                setState((){
-                              isloading=false;
-                              });
-                              Fluttertoast.showToast(msg: "Downloaded!");
-                              });
-                        
                               final DataModel data = await submitData(imageUrl);
 
-                              setState((){
-                              _dataModel = data;
-                              });
-                              
-                              
-          
-                            };
+                              setState(
+                                () {
+                                  _dataModel = data;
+                                },
+                              );
+                            }
+                            ;
                           },
-                          child: isloading? CircularProgressIndicator(color:Colors.white):Text(tSegment.toUpperCase())
-                        ),
-                      ),
-            ]
-          )
-        ),
-      )  
-    );
+                          child: isloading
+                              ? const CircularProgressIndicator(
+                                  color: Colors.white)
+                              : Text(tSegment.toUpperCase())),
+                    ),
+                  ])),
+        ));
   }
 }
 
 class ClickableIcon extends StatelessWidget {
   final auth = FirebaseAuth.instance;
   @override
-  
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        auth.signOut().then((value){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> OnBoardingScreen()));
+        auth.signOut().then((value) {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => OnBoardingScreen()));
         });
       },
       child: const Icon(
