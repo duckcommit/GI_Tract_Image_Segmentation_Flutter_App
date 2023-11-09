a65682b5e470e32fb84de71b5d9db7d8f2388084
VYSHNAV AJITH <puthen1977@gmail.com>
2023-11-09 14:29:48 +0530

    fix: Improved Performance on verify_email.dart

--- givison/lib/src/features/authentication/screens/dashboard/verify_email.dart
+++ givison/lib/src/features/authentication/screens/dashboard/verify_email.dart
@@ -2,79 +2,85 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:givison/src/features/authentication/screens/dashboard/dashboard.dart';
-import 'package:givison/src/constants/colors.dart';
 import 'package:givison/src/constants/image_strings.dart';
 import 'package:givison/src/constants/size.dart';
 import 'package:givison/src/constants/text_strings.dart';
 
 class Verify extends StatefulWidget {
-  const Verify({Key? key}):super(key:key);
-  
+  const Verify({Key? key}) : super(key: key);
+
   @override
-  
-  State<Verify> createState()=> _VerifyState();
+  State<Verify> createState() => _VerifyState();
 }
 
-class _VerifyState extends State<Verify>{
-  bool isEmailVerified=false;
+class _VerifyState extends State<Verify> {
+  bool isEmailVerified = false;
   late Timer _timer;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
 
-    if(!isEmailVerified){
+    if (!isEmailVerified) {
       sendVerificationEmail();
 
       _timer = Timer.periodic(
-        Duration(seconds:3),
-        (timer)=>checkEmailVerified(),
+        const Duration(seconds: 3),
+        (timer) => checkEmailVerified(),
       );
     }
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _timer.cancel();
     super.dispose();
   }
 
-  Future checkEmailVerified()async{
+  Future checkEmailVerified() async {
     await FirebaseAuth.instance.currentUser!.reload();
-    setState((){
+    setState(() {
       isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
     });
     if (isEmailVerified) {
       _timer.cancel();
-  }
+    }
   }
 
-  Future sendVerificationEmail()async{
+  Future sendVerificationEmail() async {
     final user = FirebaseAuth.instance.currentUser!;
     await user.sendEmailVerification();
   }
 
   @override
-  Widget build(BuildContext context)=>isEmailVerified
-    ? Dashboard()
-    : Scaffold(
-      backgroundColor: Color.fromRGBO(184, 190, 221, 1),
-      body:Container(
-        padding: EdgeInsets.all(tDefaultSize),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image(image: AssetImage(tver)),
-            Column(
+  Widget build(BuildContext context) => isEmailVerified
+      ? const Dashboard()
+      : Scaffold(
+          backgroundColor: const Color.fromRGBO(184, 190, 221, 1),
+          body: Container(
+            padding: const EdgeInsets.all(tDefaultSize),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(tVerifyTitle, style: Theme.of(context).textTheme.displayMedium,textAlign: TextAlign.center,),
-                Text(tVerifySTitle, style: Theme.of(context).textTheme.titleSmall,textAlign: TextAlign.center,),
+                const Image(image: AssetImage(tver)),
+                Column(
+                  children: [
+                    Text(
+                      tVerifyTitle,
+                      style: Theme.of(context).textTheme.displayMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      tVerifySTitle,
+                      style: Theme.of(context).textTheme.titleSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        );
 }
